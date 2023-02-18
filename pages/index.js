f8897e19c5b0a53c85fb76ab6771e7bc382c755b
Ariel Ramirez <ariel.ramirez.reyes@gmail.com>
2023-02-17 21:12:20 -0600

    Body

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Avatar from "@/Components/Avatar";
-import {Squares2X2Icon} from "@heroicons/react/20/solid";
+import {MicrophoneIcon, Squares2X2Icon} from "@heroicons/react/20/solid";
+import Image from "next/image";
+import {MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
 
 export default function Home() {
@@ -31,6 +33,18 @@ export default function Home() {
 
         </header>
         {/*Body*/}
+        <form className={'flex flex-col items-center mt-44 flex-grow'}>
+            <Image src={'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'} alt={'Google Logo'} height={100} width={300}/>
+            <div className={'flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md flex-grow rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'}>
+                <MagnifyingGlassIcon className={'h-5 mr-3 text-gray-500'}/>
+                <input type={'text'} className={'flex-grow focus:outline-none'}/>
+                <MicrophoneIcon className={'h-5'}/>
+            </div>
+            <div className={'flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'}>
+                <button className={'btn'}>Google Search</button>
+                <button className={'btn'}>I'm Feeling Lucky</button>
+            </div>
+        </form>
         {/*Footer*/}
 
 
