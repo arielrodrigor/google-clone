fb07dbe0826af729424fd72ca1acfbe1d003d47f
Ariel Ramirez <ariel.ramirez.reyes@gmail.com>
2023-02-18 21:37:38 -0600

    Init Search Page

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,20 @@ import {MicrophoneIcon, Squares2X2Icon} from "@heroicons/react/20/solid";
 import Image from "next/image";
 import {MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 import Footer from "@/Components/Footer";
+import {useRef} from "react";
+import {useRouter} from "next/router";
 
 
 export default function Home() {
+    const router = useRouter();
+    const searchInputRef = useRef(null);
+    const search = e => {
+        e.preventDefault();
+        const term = searchInputRef.current.value;
+        if (!term) return;
+        router.push(`/search?term=${term}`)
+
+    }
   return (
     <div className={'flex flex-col  justify-center items-center h-screen'}>
       <Head>
@@ -38,12 +49,12 @@ export default function Home() {
             <Image src={'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'} alt={'Google Logo'} height={100} width={300}/>
             <div className={'flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md  rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'}>
                 <MagnifyingGlassIcon className={'h-5 mr-3 text-gray-500'}/>
-                <input type={'text'} className={'flex-grow focus:outline-none'}/>
+                <input  ref={searchInputRef}  type={'text'} className={'flex-grow focus:outline-none'}/>
                 <MicrophoneIcon className={'h-5'}/>
             </div>
             <div className={'flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'}>
-                <button className={'btn'}>Google Search</button>
-                <button className={'btn'}>I'm Feeling Lucky</button>
+                <button onClick={search} className={'btn'}>Google Search</button>
+                <button onClick={search} className={'btn'}>I'm Feeling Lucky</button>
             </div>
         </form>
         {/*Footer*/}
---
+++ pages/search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from "next/head";
+function Search() {
+    return (
+        <div>
+            <Head>
+                <title>Search Page</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Search Page</h1>
+            {/*Header*/}
+            {/*Search Result*/}
+
+        </div>
+    );
+}
+
+export default Search;
