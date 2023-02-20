2b2bf1102a5d9ae2c1fe3cf1a1dbd1252c1014c9
Ariel Ramirez <ariel.ramirez.reyes@gmail.com>
2023-02-19 22:04:59 -0600

    default value

--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,7 @@ function Header() {
                     onClick={() => router.push('/')}
                     alt={'Google Logo'} height={40} width={120}/>
                 <form className={'flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center'}>
-                    <input  ref={searchInputRef}  type={'text'} className={'flex-grow focus:outline-none'}/>
+                    <input  ref={searchInputRef}  type={'text'} className={'flex-grow focus:outline-none'} defaultValue={router.query.term}/>
                     <XMarkIcon className={'h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125 '}
                                onClick={() => searchInputRef.current.value = ""}/>
                     <MicrophoneIcon className={'h-6 mr-3 text-blue-500 hidden sm:inline-flex border-l-2 pl-4 border-gray-300'}/>
